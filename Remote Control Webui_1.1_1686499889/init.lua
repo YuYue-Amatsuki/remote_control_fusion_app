0d46639503fbf20411e6be16cb6416cbc06852c1
YuYue-Amatsuki <2455107573@qq.com>
2023-06-12 00:12:04 +0800

    add source code

---
+++ Remote Control Webui_1.1_1686499889/init.lua
@@ -0,0 +1,13 @@
+packagename="org.yuyue.control.webui"
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+template="tab"
+appname="Remote Control Webui"
+appcode="101"
+appver="1.1"
---
+++ Remote Control Webui_1.1_1686499889/sub/1686493710/init.lua
@@ -0,0 +1,2 @@
+name="局域网"
+template="tab"
